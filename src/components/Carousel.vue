8093c2038e789fae957f56ca20eefea7c35d6cc1
tephan.bogahawattage <tephan.bogahawattage@epita.fr>
2024-04-09 19:37:07 +0200

    Initial Commit

---
+++ src/components/Carousel.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="myCarousel" class="carousel slide mb-6" data-bs-ride="carousel" data-bs-interval="2000">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item">
+                <img src="/rocketLeague.webp" class="img-fluid" alt="...">
+            </div>
+            <div class="carousel-item active">
+                <img src="/cs2.webp" class="img-fluid" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/lol.jpeg" class="img-fluid" alt="...">
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+    #myCarousel 
+    {
+        width: 230px;
+        height: 330px;
+    }
+
+    .carousel-indicators {
+        bottom: -50px;
+    }
+
+    .carousel-indicators button{
+        background-color: white !important;
+    }
+
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <nav class="navbar">
+        <div class="container-fluid d-flex justify-content-center">
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+
+
+</script>
+
+
+<style scoped>
+
+    input[type="search"] {
+        background: transparent;
+        width: 500px;
+        color: black;
+        outline: none !important;
+    }
+
+    input[type="search"]:focus {
+        background-color: transparent;
+        
+    }
+
+    button[type="submit"]:hover {
+        background-color: gray;
+    }
+
+    button[type="submit"] {
+        color: black;
+        border-color: black;
+    }
+
+</style>
