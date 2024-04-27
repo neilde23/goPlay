0f337cc96113da557c4da7c3aeaedfc1b3d7611b
AlanDadann <alan.delmes@efrei.net>
2024-04-27 16:59:23 +0100

    Update About.vue
    test

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -3,6 +3,9 @@
         <h1>About</h1>
         <p>goPlay is a web application that allows users to search for and book sports facilities in their area. The application is designed to be user-friendly and accessible to all users. Users can search for facilities by location, sport, and price. They can also view facility details, check availability, and book a time slot. goPlay is the perfect solution for anyone looking to stay active and have fun!</p>
     </div>
+    <div>
+        <h1>MAco</h1>
+    </div>
 </template>
 
 <script>
