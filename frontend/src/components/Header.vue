afefd627b413bd4856df96e4013dcfadc41c6188
lcdnp00 <dionyssopouloselias@gmail.com>
2024-04-27 16:39:30 +0100

    add team page

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -7,6 +7,7 @@
                 <router-link class="nav-link fw-bold py-1 px-0" to="/">Home</router-link>
                 <router-link class="nav-link fw-bold py-1 px-0" to="/player">Player area</router-link>
                 <router-link class="nav-link fw-bold py-1 px-0" to="/manager">Manager area</router-link>
+                <router-link class="nav-link fw-bold py-1 px-0" to="/player">My team</router-link>
                 <router-link class="nav-link fw-bold py-1 px-0" to="/">About</router-link>
             </div>
             <div class="user-info">
