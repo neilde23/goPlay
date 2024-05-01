b0e4d92e0239c65e93e0f8f4d0ed250e574bd98f
AlanDadann <alan.delmes@efrei.net>
2024-05-01 16:53:17 +0100

    Update About.vue

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,11 +1,28 @@
 <template>
     <div>
         <h1>About</h1>
-        <p>goPlay is a web application that allows users to search for and book sports facilities in their area. The application is designed to be user-friendly and accessible to all users. Users can search for facilities by location, sport, and price. They can also view facility details, check availability, and book a time slot. goPlay is the perfect solution for anyone looking to stay active and have fun!</p>
+        <p>
+        Go play is an online platform for managing ESports teams. <br>
+        If you're a gamer, you can now sign up and join a team. <br>
+        You will be informed of the different events which will be placed according to your timetable.  <br>
+
+        If you're a manager, everything's been done to make your job easier. <br> Manage your team, create events and make your team the best team ever. <br><br>
+
+        The whole team is at your disposal if you have any questions, just write to us below: 
+
+        Translated with DeepL.com (free version)</p>
     </div>
-    <div>
-        <h1>MAco</h1>
-    </div>
+    <form method="get" action="">
+        <label for="prénom">Firstname : </label>
+        <input type="text" name="prénom" id="prénom" placeholder="Ex.Michael">
+        <label for="nom">Name</label>
+        <input type="text" name="nom" id="nom" placeholder="Jackson">
+        <label for="Email"> Your Email</label>
+        <input type="Email" name="Email" id="Email" placeholder="MJ@Mail.com">
+
+        <label for="ameliorer"> Q&A </label><br>
+        <textarea name="ameliorer" id="ameliorer"></textarea>
+    </form>
 </template>
 
 <script>
